b65e3311200396cee8cc1c675c4b6f15777a1fa3
박지원 <jackgg12322@gmail.com>
2024-01-03 08:52:20 +0900

    feat : MongoDB 데이터 값 받아오기

---
+++ pages/api/post.js
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb';
+export default async function handler(req, res) {
+ const url = 'mongodb+srv://admin:Whdqhr12@cluster0.ebcae3z.mongodb.net/';
+const client = new MongoClient(url);
+try {
+ await client.connect();
+const db = client.db('blog');
+const posts = await db.collection('post').find().toArray();
+res.status(200).json(posts);
+} catch (error) {
+ res.status(500).json({ error: 'Unable to fetch data' });
+} finally {
+ await client.close();
+}
+}
