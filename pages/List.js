f1b6bbf2c6a644ddff1bea3cd12a5083237211d8
박지원 <jackgg12322@gmail.com>
2024-01-03 10:43:51 +0900

    feat : list Page 구현 및 DB 데이터 출력

---
+++ pages/List.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import React from 'react';
+
+export default function List() {
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('/api/post');
+      const data = await response.json();
+      setPost(data);
+    }
+    fetchData();
+  }, []);
+  return (
+    <div className="list-bg">
+        {post.map((item) => {
+          return(      
+          <div className="list-item" key={item}>
+          <h4>{item.title}</h4>
+          <p>{item.content}</p>
+          </div>
+          )
+        })
+      }
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,7 @@
-import {useEffect, useState} from "react";
-
-export default function Page() {
- const [post, setPost] = useState([]);
-useEffect(() => {
- async function fetchData() {
- const response = await fetch('/api/post');
-const data = await response.json();
-setPost(data);
-}
- fetchData();
-}, []);
+export default function Home() {
 return (
  <div>
- <h1>Post</h1>
- {post.map((post, index) => (
- <div key={index}>
- <h2>{post.title}</h2>
- <p>{post.content}</p>
- </div>
- ))}
+  HomePage
  </div>
  );
 }
