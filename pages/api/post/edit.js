f406f32ee74a7c3c8c3548d1056c87f31b3b13f4
Park Ji Won <124119421+PJW980921@users.noreply.github.com>
2024-01-04 22:02:46 +0900

    Update edit.js

--- pages/api/post/edit.js
+++ pages/api/post/edit.js
@@ -3,7 +3,7 @@ import { MongoClient, ObjectId } from 'mongodb';
 export default async function editHandler(req, res) {
   const filter = { _id: new ObjectId(req.body._id) };
   const setReqObj = {title : req.body.title , content : req.body.content}
-  const url = 'mongodb+srv://admin:blog1644@cluster0.ebcae3z.mongodb.net/';
+  const url = 'mongodb+srv://admin:aaaa@cluster0.ebcae3z.mongodb.net/';
   const client = new MongoClient(url);
 
   if (req.method === 'POST') {
