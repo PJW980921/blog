74d400a0c32fc58622fccb5eacdac9e0200884e7
박지원 <jackgg12322@gmail.com>
2024-01-04 12:08:18 +0900

    feat : 삭제 기능 구현

--- pages/List/ListItem.js
+++ pages/List/ListItem.js
@@ -1,7 +1,38 @@
-export default function ListItem() {
+'use client'
+import Link from 'next/link';
+
+export default function ListItem({post}) {
   return (
     <div>
-      
+       {post.map((item, i) => (
+        <div className="list-item" key={i}>
+          <Link href={`/detail/${item._id}`}>
+            {' '}
+            <h4>{item.title}</h4>
+          </Link>
+          <Link href={`/edit/${item._id}`}>📝 수정하기</Link>
+          <span onClick={(e)=>{
+            fetch('/api/post/delete',{
+              method : 'DELETE',
+              body : item._id,
+            }).then((r)=>{
+              if(r.status === 200){
+                return r.json();
+              }else{
+
+              }
+            }).then((success)=>{
+              e.target.parentElement.style.opacity = 0;
+              setTimeout(()=>{
+                e.target.parentElement.style.display = 'none';
+              },1000)
+            }).catch((error)=>{
+
+            })
+          }}>🗑️ 삭제하기</span>
+          <p>{item.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ pages/api/post/delete.js
@@ -0,0 +1,23 @@
+import { MongoClient,ObjectId } from 'mongodb';
+
+export default async function deleteHandler(req, res) {
+  const url = 'mongodb+srv://admin:blog1644@cluster0.ebcae3z.mongodb.net/';
+  const client = new MongoClient(url);
+  const filter = { _id: new ObjectId(req.body) };
+  if (req.method === 'DELETE') {
+    console.log(req.body);
+  }
+  if (req.body.title === '') {
+    return res.status(500).json('제목을 작성 해주세요.');
+  }
+  try {
+    await client.connect();
+    const db = client.db('blog');
+   await db.collection('post').deleteOne(filter);
+    return res.status(200).redirect('삭제 완료');
+  } catch (error) {
+    res.status(500).json({ error: '삭제하기를 실패 했습니다.' });
+  } finally {
+    await client.close();
+  }
+}
